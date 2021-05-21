61f83754bb4edde2a648209dfb3b1706893ddeaf
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-05-21 16:28:52 +0700

    Make it better

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var router = express.Router()
-const setDbConnection =  require('../tool')
 
 
 router.get('/', function(req, res, next) {
+  const setDbConnection =  require('../tool')
   setDbConnection(router)
   res.render('login', { siteTitle: 'Khmer Web TV' })
 });
